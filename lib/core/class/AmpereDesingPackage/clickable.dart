ebc732858a26beadc464c650f59f5aba8ac72194
Omar-IDOUDAOUD <pcomar.lenovo@gmail.com>
2022-10-04 00:04:08 +0100

    add clickable widget to ampere library

---
+++ lib/core/class/AmpereDesingPackage/clickable.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+typedef _builder = Widget Function(bool isFocus);
+
+class AmpereClickable extends StatefulWidget {
+  const AmpereClickable(
+      {Key? key, required this.builder, this.onTap, this.onLongPress})
+      : super(key: key);
+  final _builder builder;
+  final Function()? onTap;
+  final Function()? onLongPress;
+
+  @override
+  State<AmpereClickable> createState() => _AmpereClickableState();
+}
+
+class _AmpereClickableState extends State<AmpereClickable> {
+  bool _isFocus = false;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      // onTapDown: (dts) => setState(() {
+      //   _isFocus = true;
+      // }),
+      // onTapUp: (dts) => setState(() {
+      //   _isFocus = false;
+      // }),
+      // onTapCancel: () => setState(() {
+      //   _isFocus = false;
+      // }),
+      onTap: widget.onTap,
+      onLongPressDown: (dts) => setState(() {
+        _isFocus = true;
+      }),
+      onLongPressUp: () => setState(() {
+        _isFocus = false;
+      }),
+      onLongPressCancel: () => setState(() {
+        _isFocus = false;
+      }),
+      onLongPress: widget.onLongPress,
+      child: widget.builder(_isFocus),
+    );
+  }
+}
